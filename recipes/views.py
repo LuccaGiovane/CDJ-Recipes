d809bf792b88c2301a54733b433021182071d25a
Lucca <lucca_uel@outlook.com>
2025-01-17 14:51:52 -0300

    Advancing and factory

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'name':'Lucca Gomes'})
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)]})
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={'name':'Lucca Gomes'})
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe()})
 
