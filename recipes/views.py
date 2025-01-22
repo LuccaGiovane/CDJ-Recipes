a4b98eb9a01b340c2b57b4bbb8f9c5902f51a603
Lucca <lucca_uel@outlook.com>
2025-01-22 14:35:43 -0300

    Category filter added using get_list_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
 def home(request):
 
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
@@ -13,10 +12,16 @@ def home(request):
 
 def category(request, category_id):
 
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id, 
+            is_published=True
+            ).order_by('-id')
+        )
 
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name}  | '
     })
 
 def recipe(request, id):
